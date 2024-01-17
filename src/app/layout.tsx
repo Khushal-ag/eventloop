821460ee77300416919ff941b567c700b675a969
Khushal Agarwal <starkhush5@gmail.com>
2024-01-17 11:04:51 +0530

    init with shadcn ui

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import "@/styles/globals.css";
+
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
+export const metadata = {
+  title: "Eventloop",
+  description: "Eventloop is a platform to post and manage events",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={poppins.variable}>{children}</body>
+    </html>
+  );
+}
