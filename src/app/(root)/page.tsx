adc9e354ed435a72370493a92b483f58276530c9
Khushal Agarwal <starkhush5@gmail.com>
2024-01-21 00:04:56 +0530

    feat : home page structure

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,50 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+
 function Home() {
   return (
-    <main className="flex h-full items-center justify-center">
-      <h1 className="text-4xl">Eventloop</h1>
-    </main>
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
+        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="h1-bold">
+              Host, Connect, Celebrate: Your Events, Our Platform!
+            </h1>
+            <p className="p-regular-20 md:p-regular-24">
+              Book and learn helpful tips from{" "}
+              <a className="text-primary-500">3,168+ </a>
+              mentors in world-class companies with our global community.
+            </p>
+            <Button size="lg" asChild className="button w-full sm:w-fit">
+              <Link href="#events">Explore Now</Link>
+            </Button>
+          </div>
+          <Image
+            src="/assets/images/hero.png"
+            alt="hero"
+            width={1000}
+            height={1000}
+            className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
+          />
+        </div>
+      </section>
+      <section
+        id="events"
+        className="wrapper my-8 flex flex-col gap-8 md:gap-12"
+      >
+        <h2 className="h2-bold">
+          Trust by <br /> Thousands of Events
+        </h2>
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          <div className="text-xl font-medium text-primary-500">Search</div>
+          <div className="text-xl font-medium text-primary-500">
+            Category Filter
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
